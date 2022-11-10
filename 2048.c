007213e86d5c266d46c2a636a552da4f42c26f07
aswarth <m.aswarthreddy@gmail.com>
2022-11-10 19:22:12 +0530

    Fix issue #1

--- 2048.c
+++ 2048.c
@@ -1,12 +1,11 @@
-//COMPILE WITH C99
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 
-#define ROWS 4       //NUMBER OF ROWS
-#define COLUMNS 4       //NUMBER OF COLUMNS
+#define ROWS 4
+#define COLUMNS 4
 
-int A[ROWS][COLUMNS];    /////MAKE THIS FILL BY FOR LOOP
+int A[ROWS][COLUMNS];
 int score = 0;
 
 void show(int A[ROWS][COLUMNS]){
@@ -57,13 +56,13 @@ int up(int A[ROWS][COLUMNS]){
     for (int j=0;j<COLUMNS;j++){
         int count =0;
         int i =0;
-        while(count<ROWS && i<ROWS-1){       //maybe i<2 actually. coz shiftup(2+1) is called. and shift up also does +1. so it tries to access A[4]
+        while(count<ROWS && i<ROWS-1){
             if (A[i][j] == -1){
                 shift_up(A, i, j);
                 count++;
 
-                for (int k=i; k<=ROWS-1; k++){        //Checks if its all -1 below this -1. If so shift up is not considered a move.
-                    if (A[k][j] != -1)          //Ensures there's atleast one non -1 below
+                for (int k=i; k<=ROWS-1; k++){
+                    if (A[k][j] != -1)
                         flag = 1;
                 }
             }
@@ -72,8 +71,8 @@ int up(int A[ROWS][COLUMNS]){
                     shift_up(A,i+1,j);
                     count++;
 
-                    for (int k=i+1; k<=ROWS-1; k++){        //Checks if its all -1 below this -1. If so shift up is not considered a move.
-                        if (A[k][j] != -1)            //Ensures there's atleast one non -1 below
+                    for (int k=i+1; k<=ROWS-1; k++){
+                        if (A[k][j] != -1)
                             flag = 1;
                     }
                 }
@@ -153,8 +152,8 @@ int left(int A[ROWS][COLUMNS]){
                 shift_left(A, i, j);
                 count++;
 
-                for (int k=j; k<=COLUMNS-1; k++){        //Checks if its all -1 to the right of this -1. If so shift left is not considered a move.
-                    if (A[i][k] != -1)          //Ensures there's atleast one non -1 to the right.
+                for (int k=j; k<=COLUMNS-1; k++){
+                    if (A[i][k] != -1)          
                         flag = 1;
                 }
 
@@ -164,8 +163,8 @@ int left(int A[ROWS][COLUMNS]){
                     shift_left(A,i,j+1);
                     count++;
 
-                    for (int k=j+1; k<=COLUMNS-1; k++){        //Checks if its all -1 to the right of this -1. If so shift left is not considered a move.
-                        if (A[i][k] != -1)          //Ensures there's atleast one non -1 to the right.
+                    for (int k=j+1; k<=COLUMNS-1; k++){        
+                        if (A[i][k] != -1)          
                             flag = 1;
                     }
 
@@ -238,8 +237,8 @@ void randomSquare(int A[ROWS][COLUMNS]){
     while(1){
         for (int i=0; i<ROWS; i++){
             for (int j=0; j<COLUMNS; j++){
-                if(A[i][j] == -1 && rand()%2*ROWS*COLUMNS ==0){    //ADJUST PROBABILITY HERE. 
-                    A[i][j] = rand()%10==0?4:2;                    //HIGHER MODULUS OF RAND MEANS MORE EVEN DISTRIBUTION OF NEW NUMBERS(makes it slower ig)
+                if(A[i][j] == -1 && rand()%2*ROWS*COLUMNS ==0){
+                    A[i][j] = rand()%10==0?4:2;                
                     return;
                 }
             }
@@ -296,29 +295,21 @@ void initialise(){
 
 int main(){
     artsy();
-    srand(time(0));                 //time(0) gives current unix time. srand is used to seed rand
+    srand(time(0));                
     initialise();
     char c = 'z';
     char play = 'y';
-    while(c != 'e'){                   //e for exit
+    while(c != 'e'){                 
         show(A);
         if(isGameOver(A)){
             printf("***GAME OVER***\n");
             printf("Your score: %d\n",score);
-            // printf("Do you want to play again? y or n? ");
-            // scanf("%c",play);
-            // if (play == 'y'){
-            //     //reset();
-            //     // A = {-1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1};
-            //     // score = 0;
-            //     //main();
-            // }
             return 0;
         }
         
         printf("Current score: %d\n",score);
         printf("Make a move: ");
-        scanf(" %c",&c);     //without empty space scanf takes \n as input. No idea why. no idea where it comes from. DANGLING \n
+        scanf(" %c",&c);     
 
         switch (c)
         {
@@ -348,38 +339,3 @@ int main(){
         }
     }
 }
-//// BIG ISSUE WITH CODE> DONT ADD RANDOM IF MOVE DOESNT CHANGE ANYTHING. DUM DUM.  DONE
-
-// RANDOM INITIALISE    DONE
-
-// MAKE -1 appear as 0 by changing show function       DONE
-
-// IS GAME OVER FUNCTION              DONE
-
-// ADD CLS?
-
-// ADD play again?
-
-// Calculate score?    DONE
-
-//NOTHINGS CHANGING AND SCORE IS INCREASING?  ISGAMEOVER CALLS up down and stuff adding to score      FIXED
-
-
-
-/*
-For each column;
-
-while count<4(maybe take more just in case)
-    if -1 shift up. count++ ; else check below. 
-        if unequal. i++
-        if equal. this = double. below = -1. i++(to prevent adding again. not alowd to touch it again)
-
-        if -1. shift up for below. keep i same.    (also maybe count++)
-        
-*/
-
-// nscript doom
-
-
-
-
